e056f18244b541d7b92227e1f6907c0520b08442
RuitaoWang <wang.ruitao@outlook.com>
2022-09-13 23:23:53 +0800

    redocument for polation

--- vix/polation.py
+++ vix/polation.py
@@ -1,5 +1,5 @@
 """
-strike 内插值
+Interpolation
 """
 
 from copy import deepcopy
@@ -10,18 +10,17 @@ from scipy.interpolate import pchip_interpolate, interp1d
 
 def JiangTian_add_strike(strike_list, std, implied_forward):
     """
-    Reference: Jiang & Tian (2005)
+    Jiang & Tian (2005) Recommend to use Interpolation and Exterpolation to solve potential truncation errors and discretion errors. 
+    In particular, they recommend to polate between -8 * std ~ 8 * std to reduce the truncation error. For each two strikes, the distance
+    should be smaller than 0.35 * std to reduce the discretion error in the integration process.
     
-    确定需要插值的strike位置
-    范围为 -8 * std ~ * std 用于解决积分中存在的truncation error
-    两个strike之间相距应小于 0.35 * std 用于解决积分汇总存在的discretion error
+    std is the un-annualized standard deviation
 
-    注: std为非年化的标准差
 
     """    
     strike_list = deepcopy(strike_list)
 
-    # 需要8倍标准差的strike 宽度来获得稳健的积分估计, 如果并不足够，则补齐连孤单
+    # To obtain a robust integration as estimate, +- 8 * standard deviation of strikes is required
     lower_bound = np.exp(-8 * std) * implied_forward
     upper_bound = np.exp(8 * std) * implied_forward
 
@@ -30,13 +29,13 @@ def JiangTian_add_strike(strike_list, std, implied_forward):
     if upper_bound > max(strike_list):
         strike_list = strike_list + [upper_bound]
 
-    # 将implied_forward 插入
+    # insert implied forward as a point
     if implied_forward not in strike_list:
         strike_list += [implied_forward]
     
     strike_list = sorted(strike_list)
 
-    # 内插使得间距不大于0.35sd
+    # Define max distance between any consecutive 2 strikes
     distance = 0.35 * std * implied_forward
 
     added_strike = []
@@ -52,9 +51,9 @@ def JiangTian_add_strike(strike_list, std, implied_forward):
 
 def interpolate(strike_list, implied_vol, new_strike_list):
     """
-    内插使用 pchip(Piecewise Cubic Hermite Interpolating Polynomial)
-    外插使用 平插， 原因在于，如果使用linear的方式进行插值，经常会出现otm 价格高于 atm价格的violation。
-    因此为了保持一致性，使用平行插值，需要注意的是，在市场大涨大跌时，这种插值方法会出现一定程度上的低估的问题
+    Use Pchip (Piecewise Cubic Hermite Interpolating Polynomial) for interpolation
+    Use flat line for expolation. The reason why a conventional linear polation is not used here is that it will frequently creat OTM option with a higher price thant ATM option
+    To be noted that this method help to avoid arbitrage pattern at a cost of under-estimate VIX systematically
     """
 
     new_strike_list = sorted(new_strike_list)
@@ -62,12 +61,12 @@ def interpolate(strike_list, implied_vol, new_strike_list):
     min_real_strike = min(strike_list)
     max_real_strike = max(strike_list)
 
-    # 内部使用pchip的方式进行内插
+    # pchip for interpolation
     interior_new_strike = [x for x in new_strike_list if (x>=min_real_strike) and (x<=max_real_strike)]
     interior_new_iv = pchip_interpolate(strike_list, implied_vol, interior_new_strike)
     interior_new_iv = list(interior_new_iv)
 
-    # 外部使用平插
+    # flat line for external polation
     new_implied_vol = [interior_new_iv[0]] * len([x for x in new_strike_list if x < min_real_strike]) + interior_new_iv + \
                     [interior_new_iv[-1]] * len([x for x in new_strike_list if x > max_real_strike])
     
@@ -77,13 +76,13 @@ def interpolate(strike_list, implied_vol, new_strike_list):
     
 def time_interpolation_linear(time_remaining, vix2, days=30):
     """
-    通过内插值获得对应horizon的vix
+    Linear interpolation to convert to desired horizon
     """
-    # 排除vix2为负的（错误计算）
+    # Remove negative vix2 estimation
     time_remaining = [time_remaining[i] for i in range(len(vix2)) if vix2[i] > 0.]
     vix2 = [x for x in vix2 if x > 0.]
 
-    # 若只有1个值，则选用该值不进行插值
+    # Do not interpolate when there is only 1 snapshot
     if len(vix2) == 1:
         vix = vix2[0]
     else:
